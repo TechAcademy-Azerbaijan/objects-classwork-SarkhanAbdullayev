b202eaf964858e49de73a2ec82170f76f34ef157
Sarkhan <syako44@gmail.com>
2022-07-23 01:22:25 +0400

    Create main.js

--- task-3/main.js
+++ task-3/main.js
@@ -1,4 +1,4 @@
-let = [
+let list = [
     {marka:'Hyundai', rəng: 'Boz', anacaqNövü: 'Benzin', sürətlərQutusu: 'Avtomat', banNövü: 'Sedan', qiymət: 13000},
     {marka:'Hyundai', rəng: 'Qara', anacaqNövü: 'Dizel', sürətlərQutusu: 'Mexanika', banNövü: 'Kupe', qiymət: 43000},
     {marka:'Toyota', rəng: 'Qırmızı', anacaqNövü: 'Benzin', sürətlərQutusu: 'Avtomat', banNövü: 'Sedan', qiymət: 24000},
@@ -7,4 +7,24 @@ let = [
     {marka:'Hyundai', rəng: 'Qara', anacaqNövü: 'Dizel', sürətlərQutusu: 'Avtomat', banNövü: 'Sedan', qiymət: 33000},
     {marka:'BMW', rəng: 'Göy', anacaqNövü: 'Benzin', sürətlərQutusu: 'Mexanika', banNövü: 'Hetçbek', qiymət: 16000},
 ]
-    // Write code here
+
+
+let criteria = ["Benzin", "Avtomat", "Sedan"]
+
+
+let count = 0;
+let count3 = 0;
+
+for(let i = 0; i < list.length ; i++){
+    for(let key in list[i]){
+        if(criteria.includes(list[i][key]) || list[i][key] < 30000){
+            count3++;
+            if(count3 >= 3){
+                count++;
+                break;
+            }
+        }
+    }
+    count3 = 0;
+}
+console.log(count);
